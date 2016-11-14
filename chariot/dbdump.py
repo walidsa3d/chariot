1992f88fcdf2421efa4f45c6a1ed2c65b8b73f91
walid <walid.sa3d@gmail.com>
2016-11-15 00:21:08 +0100

    many improvements

---
+++ chariot/dbdump.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import psycopg2
+
+OPTIONS = {
+    'quote_all_fields': '',
+    'delimiter': '',
+    'quote_char': "'"
+}
+
+
+def get_db_connection():
+    pass
+
+
+def csv_dump():
+    pass
---
+++ chariot/s3uploader.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+from boto.s3.key import Key
+import boto
+from multiprocessing.pool import ThreadPool
+
+
+class S3Uploader(object):
+
+    def __init__(self,
+                 access_key=None,
+                 secret_key=None,
+                 region=None):
+        self.access_key = access_key
+        self.aws_secret_key = secret_key
+        self.aws_region = region
+        try:
+            self.conn = self.get_connection()
+        except:
+            print "Cannot connect to S3"
+            raise
+
+    def get_connection(self):
+        connection = boto.s3.connect_to_region(self.aws_region,
+                                               aws_access_key_id=self.access_key,
+                                               aws_secret_key=self.secret_key,
+                                               is_secure=True)
+        return connection
+
+    def upload_file(self, filename, filepath, bucket_name):
+        bucket = self.conn.get_bucket(bucket_name)
+        k = Key(bucket)
+        if not bucket.get_key(filename):
+            k.key = filename
+            k.set_contents_from_filename(filepath)
+        return self.get_s3_uri(bucket_name, filename)
+
+    @staticmethod
+    def get_s3_uri(bucket_name, key_name=''):
+        return 's3://{}/{}'.format(bucket_name, key_name)
+
+    def upload_directory(self, dirpath, bucket_name):
+        for fname in os.listdir(dirpath):
+            fpath = os.path.join(dirpath, fname)
+            self.upload_file(fname, fpath, bucket_name)
+
+    def parrallel_upload(self, dirpath, bucket_name, max_threads=5):
+        pool = ThreadPool(max_threads)
+        for fname in os.listdir(dirpath):
+            fpath = os.path.join(dirpath, fname)
+            res = pool.apply_async(self.upload_file, args=(fname, fpath, bucket_name))
+        return res.get()
+
+    def multi_upload(self):
+        pass
+
+    def create_bucket(self, bucket_name):
+        pass
